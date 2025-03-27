6273b5b375666ca0ef163d921efd5b5062dede8e
mohamed salah <mohamedsalahsabry88@gmail.com>
2025-03-27 17:34:06 +0200

    FixedNavBar

--- src/app/layout/pages/register/register.component.ts
+++ src/app/layout/pages/register/register.component.ts
@@ -20,11 +20,11 @@ export class RegisterComponent {
   constructor(private _AuthService:AuthService , private _Router:Router){}
 
   registerForm:FormGroup=new FormGroup({
-    name: new FormControl(null,[Validators.required,Validators.minLength(3),Validators.maxLength(20)]),
-    email: new FormControl(null,[Validators.required,Validators.email]),
-    phone: new FormControl(null,[Validators.required,Validators.pattern(/^01[0125][0-9]{8}$/)]),
-    password: new FormControl(null,[Validators.required,Validators.pattern(/^\w{6,}$/)]),
-    rePassword: new FormControl(null),
+      name: new FormControl(null,[Validators.required,Validators.minLength(3),Validators.maxLength(20)]),
+      email: new FormControl(null,[Validators.required,Validators.email]),
+      phone: new FormControl(null,[Validators.required,Validators.pattern(/^01[0125][0-9]{8}$/)]),
+      password: new FormControl(null,[Validators.required,Validators.pattern(/^\w{6,}$/)]),
+      rePassword: new FormControl(null),
   },this.checkPasswordData)
 
   sendData():void
